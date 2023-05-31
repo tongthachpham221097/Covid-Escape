351bd9727a6d271e22f7dee249d605eeff081906
Your Name <you@example.com>
2023-05-31 08:25:28 +0700

    Done ObstacleSpawner PlayerCtrl and fix velocity player

--- Assets/_Data/Player/PlayerCtrl.cs
+++ Assets/_Data/Player/PlayerCtrl.cs
@@ -6,5 +6,35 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class PlayerCtrl : LoboBehaviour
 {
-    
+    private static PlayerCtrl instance;
+    public static PlayerCtrl Instance { get => instance; }
+
+    [SerializeField] protected PlayerCollider playerCollider;
+    public PlayerCollider PlayerCollider { get => playerCollider; }
+
+    [SerializeField] protected PlayerMovement playerMovement;
+    public PlayerMovement PlayerMovement { get => playerMovement; }
+    protected override void Awake()
+    {
+        base.Awake();
+        if (PlayerCtrl.instance != null) Debug.LogError("only 1 PlayerCtrl allow to exist");
+        PlayerCtrl.instance = this;
+    }
+    protected override void LoadComponents()
+    {
+        this.LoadPlayerCollider();
+        this.LoadPlayerMovement();
+    }
+    protected virtual void LoadPlayerCollider()
+    {
+        if (this.playerCollider != null) return;
+        this.playerCollider = GetComponentInChildren<PlayerCollider>();
+        Debug.Log(transform.name + ": LoadPlayerCollider", gameObject);
+    }
+    protected virtual void LoadPlayerMovement()
+    {
+        if (this.playerMovement != null) return;
+        this.playerMovement = GetComponentInChildren<PlayerMovement>();
+        Debug.Log(transform.name + ": LoadPlayerMovement", gameObject);
+    }
 }
